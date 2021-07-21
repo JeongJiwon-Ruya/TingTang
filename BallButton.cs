4749fe3709ba9247204cf4424b81e051466d2cf8
JeongJiwon <eetr3295@gmail.com>
2021-07-22 02:05:22 +0900

    first commit

---
+++ BallButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("BTN"+transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ BallController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour
+{
+    Vector3 circlePosition;
+
+    public GameObject ball;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(transform.position);
+        circlePosition = this.transform.position;
+        GetComponent<Button>().onClick.AddListener(Click);
+    }
+
+    private void Update() {
+        transform.eulerAngles = new Vector3(0,0, ball.transform.eulerAngles.z);
+    }
+
+    void Click(){
+        Debug.Log("Click event occur.");
+        Vector3 expos = Input.mousePosition;
+        Vector3 npos = Camera.main.ScreenToWorldPoint(expos);
+        Vector3 fpos = new Vector3(npos.x, npos.y);
+        Debug.Log(fpos - circlePosition);
+        ball.GetComponent<Rigidbody2D>().AddForce(-(fpos-circlePosition)*550);
+ 
+    }
+         
+
+}
